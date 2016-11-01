fc0971f099bf05a83ec6552fa1b2dc3bc39321f6
Elizabet Oliveira <elizabet.oliveira@gmail.com>
2016-11-01 12:58:21 +0000

    Included redux logger

--- src/js/index.js
+++ src/js/index.js
@@ -3,18 +3,26 @@ import ReactDOM from "react-dom";
 import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerMiddleware, push } from 'react-router-redux';
 import routes from './routes';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 
 // include the stylesheet entry point
 require('../sass/app.scss');
 
+const middlewares = [thunk, routerMiddleware(browserHistory)];
+
+if (process.env.NODE_ENV === 'development') {
+	const createLogger = require('redux-logger');
+	const logger = createLogger();
+	middlewares.push(logger);
+}
+
 const store = createStore(
 	reducers,
-	applyMiddleware(thunk),
-	applyMiddleware(routerMiddleware(browserHistory))
+	compose(applyMiddleware(...middlewares))
 );
 
 const history = syncHistoryWithStore(browserHistory, store);
